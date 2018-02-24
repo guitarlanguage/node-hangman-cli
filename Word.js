f5faa9065482c24fabac4f716003ccb2564eab4f
Tom LaChance <guitarlanguageseries@gmail.com>
2018-02-23 22:36:48 -0600

    testing a Word for random word from wordbank

--- Word.js
+++ Word.js
@@ -3,26 +3,30 @@
 // constructor.
 var Letter = require("./Letter.js");
 
-var Word = function(word) {
+var Word = function() {
     //An array of `new` Letter objects representing the letters of the underlying word
-    this.letters = [];
-    this.word = word;
+//     this.letters = [];
+//     this.word = word;
+//
+//     //put those word letters into the this.letters array
+//     for (var w = 0; w < letters.length; w++) {
+//         var letter = new Letter(word[w]);
+//         this.letters.push(letter);
+//     }
+//
+// //A function that returns a string representing the word. This should call the
+// // function on each letter object (the first function defined in `Letter.js`)that
+// // displays the character or an underscore and concatenate those together.
+//     this.returnAString = function() {
+//     //A function that returns a string representing the word.
+//     var theWord = "";
+//     this.checkIt();
+//     };
 
-    //put those word letters into the this.letters array
-    for (var w = 0; w < letters.length; w++) {
-        var letter = new Letter(word[w]);
-        this.letters.push(letter);
-    }
 
-//A function that returns a string representing the word. This should call the
-// function on each letter object (the first function defined in `Letter.js`)that
-// displays the character or an underscore and concatenate those together.
-    this.returnAString = function() {
-    //A function that returns a string representing the word.
-    var theWord = "";
-    this.checkIt();
-    };
-
+var randomWord = words[Math.floor(words.length * Math.random())];
+console.log(randomWord);
+return randomWord;
 
 };
 
@@ -31,8 +35,6 @@ var words = ["carbon", "hydrogen", "helium", "gallium", "zinc", "argon", "tin",
 "lithium", "magnesium", "radium", "palladium", "silver", "lead", "krypton",
 "neon", "boron", "silicon", "iron", "chromium", "hydrogen"];
 
-console.log(this.letters);
-
-console.log(Word);
+Word();
 
 module.exports = Word;
